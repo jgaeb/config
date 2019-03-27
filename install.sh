66e9549edd9f1802c678ad99d0f79f14ee33e5cb
Johann Gaebler <johann.gaebler@gmail.com>
2019-03-27 00:12:15 -0700

    Fix install.sh so that symlinks can be updated.

--- install.sh
+++ install.sh
@@ -9,14 +9,28 @@ WRONG_ARGS=1
 if [[ ! $# -ge 1 ]]
 then
 	echo "Usage: ./install.sh DOTFILE [DOTFILE [DOTFILE ...]]" 1>&2
+	echo "       ./install.sh -a" 1>&2
 	exit "$WRONG_ARGS"
 fi
+
 
 BASEDIR=$(dirname $0)
 
 cd "$BASEDIR"
 
-for f in "$@"
-do
-	ln -s "${PWD}/${f}" "${HOME}/.${f}"
-done
+if [[ $1 == "-a" ]]
+then
+	for f in *
+	do
+		if [[ "$f" != "$0" ]]
+		then
+			ln -shi "${PWD}/${f}" "${HOME}/.${f}"
+		fi
+	done
+
+else
+	for f in ${FILES-@}
+	do
+		ln -shi "${PWD}/${f}" "${HOME}/.${f}"
+	done
+fi
