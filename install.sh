caae8cd54ec14d86f77ea403e4d8262e40077aaa
Johann D. Gaebler <me@jgaeb.com>
2026-02-14 10:16:18 -0500

    Make the installation script more robust.

--- install.sh
+++ install.sh
@@ -25,10 +25,38 @@ else
 	FILES="$@"
 fi
 
+symlink_one() {
+	local source="$1"
+	local target="$2"
+
+	if [[ -L "$target" ]]; then
+		# Already a symlink -- replace it
+		ln -sfn "$source" "$target"
+	elif [[ -d "$source" && -d "$target" ]]; then
+		# Both are real directories -- merge by symlinking contents
+		for child in "$source"/*; do
+			symlink_one "$child" "$target/$(basename "$child")"
+		done
+	elif [[ -e "$target" ]]; then
+		# Exists but is a real file -- ask before replacing
+		echo "$target exists and is not a symlink. Replace? [y/N]"
+		read -r reply
+		if [[ "$reply" =~ ^[Yy]$ ]]; then
+			rm -rf "$target"
+			ln -s "$source" "$target"
+		else
+			echo "Skipping $target"
+		fi
+	else
+		# Doesn't exist -- create symlink
+		ln -s "$source" "$target"
+	fi
+}
+
 for f in ${FILES[*]}
 do
 	if [[ $( basename "$f" ) != $( basename "$0" ) ]]
 	then
-		ln -sni "${PWD}/${f%/}" "${HOME}/.${f%/}"
+		symlink_one "${PWD}/${f%/}" "${HOME}/.${f%/}"
 	fi
 done
